d86b370086394d71a3705475d20aa0dd8d317078
shuaiZe <vuejs1024@163.com>
2020-06-12 15:11:40 +0800

    setstate

--- demo1/src/pages/Home.js
+++ demo1/src/pages/Home.js
@@ -4,9 +4,11 @@ export default class Home extends Component {
     constructor(props) {
         super(props)
         //使用React.StrictMode的话 方法在开发环境中constructor会执行两次
-        console.log('111111', props)
+        // console.log('111111', props)
+        //使用state维护状态，在构造函数中初始化属性
         this.state = {
-            date: new Date() || ''
+            date: new Date() || '',
+            count: 0
         }
         console.log('22222', this)
     }
@@ -15,9 +17,27 @@ export default class Home extends Component {
         console.log('看这里', this)
         this.timerId = setInterval(() => {
             this.setState({
-                date: new Date()
+                date: new Date(),
             })
         }, 1000)
+    }
+
+    counter = () => {
+        // 同步
+        //或者使用传个参数，使用回调函数实现
+        // setTimeout(() => {
+        //使用settimeout使setstate变成同步
+        //     this.setState({
+        //         count: this.state.count + 2
+        //     })
+        //     console.log('state', this.state.count)
+        // }, 0)
+
+
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log('count计数', this.state.count)
     }
 
     componentWillUnmount () {
@@ -27,9 +47,11 @@ export default class Home extends Component {
 
     render () {
         const str = 'home页面'
-        const { date } = this.state
+        const { date, count } = this.state
         return <div>
             <p>{str}</p>
+            <p>{count}</p>
+            <button onClick={this.counter}>点击增加次数</button>
             <p>{date.toLocaleString().replace(/[年月]/g, '/')}</p>
         </div>
     }
